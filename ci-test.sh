f18aac9f5ee07fc24a05a4e934ee0a110ebb162e
alokforros <44690566+alokforros@users.noreply.github.com>
2021-03-22 13:02:54 +0800

    Update ci-test.sh

--- ci-test.sh
+++ ci-test.sh
@@ -9,10 +9,12 @@ export ECS_DB_LOC=error_classification_server/ecs.db
 echo "Checking ECS API Val"
 echo $ECS_API
 
-
-pip3 install uvloop==0.14.0
+pip3 install wheel
 pip3 install sanic==20.12.0
 pip3 install databases[sqlite]==0.2.6
+
+pip3 uninstall sqlalchemy
+pip3 install sqlalchemy==1.3.13
 #pip3 install -r error_classification_server/requirements.txt
 python3 error_classification_server/src/ecs_endpoint.py & APP_PID=$!
 

f48eb64ff7f0a43017727894f4b6c609dbb93b2e
alokforros <44690566+alokforros@users.noreply.github.com>
2021-03-22 12:49:50 +0800

    Update ci-test.sh

--- ci-test.sh
+++ ci-test.sh
@@ -11,7 +11,9 @@ echo $ECS_API
 
 
 pip3 install uvloop==0.14.0
-pip3 install -r error_classification_server/requirements.txt
+pip3 install sanic==20.12.0
+pip3 install databases[sqlite]==0.2.6
+#pip3 install -r error_classification_server/requirements.txt
 python3 error_classification_server/src/ecs_endpoint.py & APP_PID=$!
 
 
